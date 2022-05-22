fa2bf5f248c647cc28ed934aae2ac349b594f71d
karen-yquevedo <kyamilequevedo@gmail.com>
2022-05-22 19:55:16 +0200

    add dom

---
+++ dom.js
@@ -0,0 +1,30 @@
+
+ /**Iteración #1: Interacción con el DOM**
+
+ Dado el siguiente HTML:
+ 
+ 
+ 
+ 1.1 Usa querySelector para mostrar por consola el botón con la clase .**showme**
+ 
+ 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+
+ document.querySelectorAll('#pillado')
+
+ 1.3 Usa querySelector para mostrar por consola todos los p
+ document.querySelectorAll('p')
+
+ 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+ document.querySelectorAll('.pokemon')
+
+ 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+ data-function="testMe".
+ let data = document.querySelectorAll('data-function')
+
+ data.forEach
+ƒ forEach() { [native code] }
+
+ 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+ data-function="testMe".*/
+
+ 
